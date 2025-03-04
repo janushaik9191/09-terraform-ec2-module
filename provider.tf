c95d5490162cccf6c3c4c88a7774c23b7f6efef3
meerjan <janushaik9191@gmail.com>
2025-03-04 23:07:54 +0530

    terraform-aws-ec2-resource and module

--- provider.tf
+++ provider.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "5.88.0"
+      source  = "hashicorp/aws"
+      version = "5.88.0"
     }
   }
-  backend "s3"{
-    bucket = "daws-s3-remote"
-    key = "ec2-module-demo"
+  backend "s3" {
+    bucket         = "daws-s3-remote"
+    key            = "ec2-module-demo"
     dynamodb_table = "daws-dynamodb"
-    region = "us-east-1"
+    region         = "us-east-1"
   }
 }
 provider "aws" {
